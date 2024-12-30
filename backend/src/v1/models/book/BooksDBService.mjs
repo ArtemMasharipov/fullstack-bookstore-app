9f523f1aa8399e3b669fe3f9d3024ccba6893769
Artem <artemmasharipov1987@gmail.com>
2024-12-30 14:03:25 +0100

    Refactor BooksDBService to extend MongooseCRUDManager for improved CRUD operations

--- backend/src/v1/models/book/BooksDBService.mjs
+++ backend/src/v1/models/book/BooksDBService.mjs
@@ -1,25 +1,9 @@
+import MongooseCRUDManager from '../MongooseCRUDManager.mjs';
 import Book from './bookModel.mjs';
 
-class BooksDBService {
-  async createBook(data) {
-    const book = new Book(data);
-    return book.save();
-  }
-
-  async getBooks() {
-    return Book.find();
-  }
-
-  async getBookById(id) {
-    return Book.findById(id);
-  }
-
-  async updateBook(id, data) {
-    return Book.findByIdAndUpdate(id, data, { new: true });
-  }
-
-  async deleteBook(id) {
-    return Book.findByIdAndDelete(id);
+class BooksDBService extends MongooseCRUDManager {
+  constructor() {
+    super(Book);
   }
 }
 
