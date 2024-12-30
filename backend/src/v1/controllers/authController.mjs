a04ddfeeaa846c75f5a4e10dc2eb44e416b56ce3
Artem <artemmasharipov1987@gmail.com>
2024-12-30 14:02:48 +0100

    Refactor authController to manage authors with CRUD operations and improve error handling

--- backend/src/v1/controllers/authController.mjs
+++ backend/src/v1/controllers/authController.mjs
@@ -1,91 +1,69 @@
-import UsersDBService from '../models/user/UsersDBService.mjs';
-import RolesDBService from '../models/role/RolesDBService.mjs';
-import { prepareToken } from '../../../services/jwtHelpers.mjs';
+import AuthorsDBService from '../models/author/AuthorsDBService.mjs';
 import { validationResult } from 'express-validator';
 
-class AuthController {
-  static async login(req, res) {
-    const { email, password } = req.body;
+export const getAllAuthors = async (req, res) => {
+  try {
+    const authors = await AuthorsDBService.getList();
+    res.json(authors);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-    if (!email || !password) {
-      return res.status(400).json({ error: 'Email and Password are required' });
+export const getAuthorWithBooks = async (req, res) => {
+  try {
+    const author = await AuthorsDBService.getAuthorWithBooks(req.params.id);
+    if (!author) {
+      return res.status(404).json({ error: 'Author not found' });
     }
+    res.json(author);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-    try {
-      const user = await UsersDBService.findByEmail(email);
-      if (!user || !(await user.isValidPassword(password))) {
-        return res.status(401).json({ error: 'Invalid email or password' });
-      }
-
-      const token = prepareToken({
-        id: user._id,
-        roleId: user.role._id,
-        permissions: user.role.permissions,
-      });
-
-      res.status(200).json({
-        message: 'Login successful',
-        token,
-        user: {
-          ...user.toObject(),
-          permissions: user.role.permissions,
-        },
-      });
-    } catch (error) {
-      console.error('Login error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+export const createAuthor = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
 
-  static async register(req, res) {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const { username, email, password, confirmPassword } = req.body;
-
-    if (password !== confirmPassword) {
-      return res.status(400).json({ error: 'Passwords do not match' });
-    }
-
-    try {
-      const userExists = await UsersDBService.findByEmail(email);
-      if (userExists) {
-        return res.status(409).json({ error: 'User already exists' });
-      }
-
-      const assignedRole = await RolesDBService.findOne({ name: 'user' });
-      if (!assignedRole) {
-        return res.status(400).json({ error: 'Default role not found' });
-      }
-
-      const newUser = await UsersDBService.createUser({
-        username,
-        email,
-        password,
-        roleName: assignedRole.name,
-      });
-
-      const token = prepareToken({
-        id: newUser._id,
-        roleId: assignedRole._id,
-        permissions: assignedRole.permissions,
-      });
-
-      res.status(201).json({
-        message: 'User registered successfully',
-        user: {
-          ...newUser.toObject(),
-          permissions: assignedRole.permissions,
-        },
-        token,
-      });
-    } catch (error) {
-      console.error('Registration error:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const newAuthor = await AuthorsDBService.create(req.body);
+    res.status(201).json(newAuthor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
-export default AuthController;
+export const updateAuthor = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const updatedAuthor = await AuthorsDBService.update(
+      req.params.id,
+      req.body,
+    );
+    if (!updatedAuthor) {
+      return res.status(404).json({ error: 'Author not found' });
+    }
+    res.json(updatedAuthor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteAuthor = async (req, res) => {
+  try {
+    const deletedAuthor = await AuthorsDBService.deleteById(req.params.id);
+    if (!deletedAuthor) {
+      return res.status(404).json({ error: 'Author not found' });
+    }
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
